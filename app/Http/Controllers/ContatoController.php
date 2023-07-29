44d47c84a8836b79ad1ae2e3525ca9a8dcd7ec11
ismael <ismaelguedessilva@gmail.com>
2023-07-29 16:46:57 -0300

    request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
-        var_dump($_POST);
+        
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+       // print_r($contato->getAttributes());
+       $contato->save();
+
+       //outro metodo 
+       /*$contato2 = new SiteContato();
+       $contato2->fill($request->all());
+       pra que de certo, precisameos add o protected fillable la na model
+       
+       ou
+
+       ->create ou inves de ->fill
+       */
+
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
