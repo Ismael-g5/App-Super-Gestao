d54333dcce1248a069ac6ae76b679f04b045d12b
ismael <ismaelguedessilva@gmail.com>
2023-08-05 12:39:08 -0300

    adição resource route

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,15 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(
     Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name('app.fornecedor.excluir');
 
     //produtos
-    Route::resource('produto', 'ProdutoController');
+    Route::resource('produtos', ProdutoController::class)->names([
+        'index' => 'produto.listar',      // Nome da rota para a listagem de produtos
+        'create' => 'produto.criar',      // Nome da rota para a página de criação
+        'store' => 'produto.salvar',      // Nome da rota para salvar um novo produto
+        'show' => 'produto.mostrar',      // Nome da rota para exibir um produto específico
+        'edit' => 'produto.editar',       // Nome da rota para a página de edição
+        'update' => 'produto.atualizar',  // Nome da rota para atualizar um produto existente
+        'destroy' => 'produto.excluir',   // Nome da rota para excluir um produto
+    ]);
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
